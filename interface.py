ee30fe18f38a4e501e0da991639a8a65841f6daa
CristianAlarcon <48411410+CristianAlarcon@users.noreply.github.com>
2023-07-21 13:24:58 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -10,8 +10,7 @@ loaded_model = joblib.load('rforest.joblib')
 # Obtener la versión de Joblib
 joblib_version = joblib.__version__
 
-# Mostrar la versión de Joblib en la aplicación de Streamlit
-st.write("Versión de Joblib:", joblib_version)
+
 # Nombres de las características
 feature_names = ['Caracteristicas', 'Habitaciones', 'Aseos', 'Terraza',
                  'Piscina', 'Garaje', 'Metros', 'Barri', ' Poblacio_ocupada',
@@ -19,7 +18,7 @@ feature_names = ['Caracteristicas', 'Habitaciones', 'Aseos', 'Terraza',
 
 # Interfaz de Streamlit
 st.title("Predicció d'Immobles a Barcelona")
-st.write(
+st.write("Versión de Joblib:", joblib_version)
 st.write('Introdueix els valors de les característiques y prem el botó '
          'corresponent.')
 caracteristicas_valores = ('Apartament', 'Àtic', 'Finca Rústica', 'Duplex',
